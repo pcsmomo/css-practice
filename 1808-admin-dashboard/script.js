1749d0ccf84bda84058566f45a3ff6c41a37eb37
pcsmomo <dongwoo_333@hanmail.net>
2022-01-15 21:12:29 +1100

    1808. Create Responsive Admin Dashboard: main topbar

--- 1808-admin-dashboard/script.js
+++ 1808-admin-dashboard/script.js
@@ -1,3 +1,13 @@
+// MenuToggle
+const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+const main = document.querySelector('.main');
+
+toggle.onclick = function () {
+  navigation.classList.toggle('active');
+  main.classList.toggle('active');
+};
+
 // add hovered class in selected list item
 const list = document.querySelectorAll('.navigation li');
 
